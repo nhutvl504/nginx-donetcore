b41faa935f1b9dac26c01614a661e3d58bfdbfb1
nhutvl504 <nhutlmse60607@gmail.com>
2018-02-22 13:51:08 +0900

    Update push-ecr.sh

--- push-ecr.sh
+++ push-ecr.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env sh
 
 eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
-docker push $DEV_AWS_ECR_URI:$CIRCLE_SHA1
+docker push $DEV_AWS_ECR_URI:$CIRCLE_BUILD_NUM
 
 sleep 20
 exit 0
